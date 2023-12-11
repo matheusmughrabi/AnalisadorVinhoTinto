83519c5b72014b3920b1c53877c9c76fd7e7322f
matheuscm (Matheus Campanini) <matheus.campanini@ploomes.com>
2023-12-11 00:07:05 -0300

    Melhorias no formulário

--- src/WebApp/scripts.js
+++ src/WebApp/scripts.js
@@ -1,4 +1,28 @@
+const limitesAceitaveis = {
+    fixedAcidity: { min: 7.1, max: 9.2 },
+    volatileAcidity: { min: 0.39, max: 0.64 },
+    chlorides: { min: 0.07, max: 0.09 },
+    totalSulfurDioxide: { min: 22, max: 62 },
+    sulphates: { min: 0.62, max: 0.73 },
+    alcohol: { min: 9.5, max: 10.2 }
+};
+
 $(document).ready(function() {
+    Object.keys(limitesAceitaveis).forEach(campo => {
+        $(`#${campo}`).on('input', function() {
+            const valor = parseFloat($(this).val());
+            const { min, max } = limitesAceitaveis[campo];
+
+            if (valor < min || valor > max) {
+                // Mostra o alerta se o valor estiver fora dos limites
+                $(`#${campo}Alert`).remove();
+                $(this).after(`<p id="${campo}Alert" style="color: red;">Valor fora do limite aceitável de ${min} - ${max}</p>`);
+            } else {
+                $(`#${campo}Alert`).remove();
+            }
+        });
+    });
+
     $('#wineQualityForm').on('submit', function(e) {
         e.preventDefault(); // Impede o envio normal do formulário
 
